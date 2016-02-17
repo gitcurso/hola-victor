424eedfd957455c816af9898610aba67aeaa1fdd
Plegue <plegue@mail.es>
2016-02-17 12:50:01 +0100

    presentacion
    Una parte
    
    2da parrte

--- new/presentacion.java
+++ new/presentacion.java
@@ -1,13 +1,20 @@
 public class presentacion {
 	
-	private static String nombre = "B";
-	private static int edat = 18;
+	private static String nombre = "A";
+	private static String nombre2 = "A2";
+	private static int edat = 17;
+	private static int hola = "hola";
 
 	
 	public static void main(String[] zz){
 
-		System.out.println("Hola mundo " + nombre + "tienes" + edat + "años de edad");
-
+		System.out.println(hola + "mundo " + nombre + "tienes" + edat + "años");
+		Map<String, Object> mp = null;
+		Iterator it = mp.entrySet().iterator();
+		while (it.hasNext()) {
+			Map.Entry pairs = (Map.Entry)it.next();
+			System.out.println(pairs.getKey() + " = " + pairs.getValue());
+			it.remove(); // avoids a ConcurrentModificationException
+		}
 	}
-
 }
