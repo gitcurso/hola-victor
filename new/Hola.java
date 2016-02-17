24977e09e711861caa1bf742cc65643367185ce2
Antonio Gonzalez <agj@git>
2016-02-17 10:33:18 +0100

    Several names

--- new/Hola.java
+++ new/Hola.java
@@ -1,10 +1,12 @@
 public class Hola {
 	
-	private static String NOMBRE = "Pato Pajaroto";
+	private static String[] NOMBRE = {"Pato Pajaroto", "otro"};
 	
 	public static void main(String[] zz){
+		
+		int n = (int)System.currentTimeMillis() % 2;
 
-		System.out.println("Hola mundo " + NOMBRE);
+		System.out.println("Hola mundo " + NOMBRE[n]);
 
 	}
 
