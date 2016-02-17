f189fd3a8faade1f08f55ddc5e100711be9b8a06
Albert Perez <albertp@mail.com>
2016-02-17 10:31:58 +0100

    Bug arreglo 1

--- new/Hola.java
+++ new/Hola.java
@@ -1,11 +1,20 @@
+import java.util.*;
+
 public class Hola {
 	
 	private static String nombre = "Pato Pajaroto";
 	
 	public static void main(String[] zz){
+		ArrayList<String> lista = new ArrayList<String>();
 
+		lista.add("casa");
+		lista.add("pajaro");
+		lista.add("coche");
+		
 		System.out.println("Hola mundo " + nombre);
-
+		
+		for (String cosa : lista) {
+			System.out.println("Hola " + cosa);
+		}
 	}
-
 }
