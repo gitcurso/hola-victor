33a526334ca250a0e0823973059235d00a8ba9b0
Antonio Gonzalez <agj@git>
2016-02-17 10:30:32 +0100

    Follow sonar rule for constants

--- new/Hola.java
+++ new/Hola.java
@@ -1,10 +1,10 @@
 public class Hola {
 	
-	private static String nombre = "Pato Pajaroto";
+	private static String NOMBRE = "Pato Pajaroto";
 	
 	public static void main(String[] zz){
 
-		System.out.println("Hola mundo " + nombre);
+		System.out.println("Hola mundo " + NOMBRE);
 
 	}
 
