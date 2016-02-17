417dac3ca4050e4741e5bd46f2467594e28cc8bc
Manel <manel@example.com>
2016-02-17 12:56:07 +0100

    This is a combination of 5 commits. mis cambios otra vez
    primera parte del cambio
    
    Segunda parte del cambio
    
    Más cambios
    
    maaaaaaas cambios !!!!

--- new/Hola.java
+++ new/Hola.java
@@ -1,26 +1,23 @@
 import java.util.*;
 
-public class Hola {
+public class Adios {     
 
 public static String nombre;	
 	public static void main(String[] zz){
-		ArrayList<String> lista = new ArrayList<String>();
+		ArrayList<String> lista = new ArrayList<String>();     
 
 		lista.add("casa");
-		lista.add("pajaro");
+		lista.add("pajaro loco");
 		lista.add("coche");
 		
 		Integer resultado = suma();		
-		System.out.println("Hola mundo " + nombre + " " + resultado.toString());
-		System.out.println("Hola mundo " + nombre);
+		System.out.println("Adios mundo " + nombre + " " + resultado.toString());     
+		System.out.println("Adios mundo " + nombre);     
 
-
-		
 		for (String cosa : lista) {
-			System.out.println("Hola " + cosa);
+			System.out.println("Adios " + cosa);
 		}
-		System.out.println("Hola mundo2 " + nombre);
-
+                System.out.println("Adios mundo2 " + nombre);
 
 	}
 	
@@ -34,6 +31,10 @@ public static String nombre;
 		
 		return 2 * 2;
 		
+	}
+
+	public static void kk() {
+		System.out.println("kk");
 	}
 }
 
--- new/Viva.java
+++ new/Viva.java
@@ -1,6 +1,6 @@
 public class Viva {
 	
-	private static String nombre2 = "Viva yoooooooooo";
+	private static String nombre = "Viva pato";
 	
 	public static void main(String[] zz){
 
