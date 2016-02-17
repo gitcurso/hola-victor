2b61aed2e4bebf4e03f38e20e8dbd8a3097b52ec
Albert Perez <albertp@mail.com>
2016-02-17 10:39:30 +0100

    Bug arreglo 2

--- new/Adios.java
+++ new/Adios.java
@@ -1,11 +1,10 @@
 public class Adios {
 	
-	private static String nombre = "Name";
+	private static String nombre = "Pepe";
 	
 	public static void main(String[] zz){
 
 		System.out.println("Adios mundo " + nombre);
 
 	}
-
 }
