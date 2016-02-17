7efe95c7ecb99a9a40d34a1ba2953d3a3ece875c
Irene Gris <irene@pue.es>
2016-02-17 10:33:55 +0100

    for

--- new/Hola.java
+++ new/Hola.java
@@ -1,10 +1,10 @@
 public class Hola {
 	
-	private static String nombre = "Pato Pajaroto";
+	private static String nombre = "Don Pepito";
 	
 	public static void main(String[] zz){
 
-		System.out.println("Hola mundo " + nombre);
+		System.out.println("Hola " + nombre+ );
 
 	}
 
