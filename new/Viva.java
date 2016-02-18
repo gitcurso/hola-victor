0cef4479537c09d7128a24dc240e40e6aeb84d79
Victor Izquierdo <yo@mail.es>
2016-02-18 12:38:13 +0100

    added pandereta

--- new/Viva.java
+++ new/Viva.java
@@ -1,6 +1,6 @@
 public class Viva {
 	
-	private static String nombre = "Viva la pepaaaaaa";
+	private static String nombre = "Viva la pepaaaaaa y la panderetaa";
 	
 	public static void main(String[] zz){
 
