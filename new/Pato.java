faac31d2611b4ca69aa2d96fcc413be4230f3d09
Manel <manel@example.com>
2016-02-18 12:41:58 +0100

    con cambioss

--- new/Pato.java
+++ new/Pato.java
@@ -1,6 +1,11 @@
 public class Pato {
 	private Pato() {}
 
+	public static void zzz(int n) {
+	        for (int i = 0; i < n; i++) {
+		System.out.println("zzzz ...");
+		}
+	}
 	public static void zzz() {
 		System.out.println("zzzz ...");
 	}
