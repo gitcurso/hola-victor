78d4e0167dd56a5f6b38602f02da2b3fe3abc206
Manel <manel@example.com>
2016-02-18 12:41:58 +0100

    con cambioss
    Conflicts:
    	new/Pato.java

---
+++ new/Pato.java
@@ -0,0 +1,13 @@
+public class Pato {
+	private Pato() {}
+
+	public static void zzz(int n) {
+	        for (int i = 0; i < n; i++) {
+		System.out.println("zzzz ...");
+		}
+	}
+	public static void zzz() {
+		System.out.println("zzzz ...");
+	}
+}
+
