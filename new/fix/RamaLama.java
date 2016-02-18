3c94b5861cb3bbb6b5eb4844bc7b0a13524ec173
Albert Perez <albertp@mail.com>
2016-02-18 12:42:55 +0100

    Added some changes to rama lama

--- new/fix/RamaLama.java
+++ new/fix/RamaLama.java
@@ -1,5 +1,6 @@
 public class RamaLama {
 	public static void main(String[] args) {
-		System.out.println("RamaLama din don");
+		System.out.println("Rama Lama din don");
+		System.out.println("Lama Rama din don");
 	}
 }
