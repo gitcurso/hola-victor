2ac25484d5040278d401e20249a7ae6ed782a460
Plegue <plegue@mail.es>
2016-02-18 12:38:01 +0100

    Added plegue file

---
+++ new/plegue.java
@@ -0,0 +1,20 @@
+public class plegue {
+	
+	private static String nombre = "A";
+	private static String nombre2 = "A2";
+	private static int edat = 17;
+	private static int hola = "hola";
+
+	
+	public static void main(String[] zz){
+
+		System.out.println(hola + "mundo " + nombre + "tienes" + edat + "años");
+		Map<String, Object> mp = null;
+		Iterator it = mp.entrySet().iterator();
+		while (it.hasNext()) {
+			Map.Entry pairs = (Map.Entry)it.next();
+			System.out.println(pairs.getKey() + " = " + pairs.getValue());
+			it.remove(); // avoids a ConcurrentModificationException
+		}
+	}
+}
