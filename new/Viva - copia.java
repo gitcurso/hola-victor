6a0081289385c94271322fb9964defb9ed89608d
Negin <negin@blabla.com>
2016-02-18 12:38:43 +0100

    add viva copia

---
+++ new/Viva - copia.java
@@ -0,0 +1,11 @@
+public class Viva {
+	
+	private static String nombre = "NEGIN COPIA";
+	
+	public static void main(String[] zz){
+
+		System.out.println("i am saying VIVA COPIA" + nombre);
+
+	}
+
+}
