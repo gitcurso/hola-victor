03e7f364c8ad95d01d2fb11fd5340845fa7c2edc
Amador <guitxo@gmail.com>
2016-02-18 09:46:36 +0100

    prueba.java

---
+++ new/prueba.java
@@ -0,0 +1,24 @@
+public class prueba {
+
+	public static String nombre;	
+	
+	private static String nombre = "Amador";	
+	
+	public static void main(String[] zz){
+		
+		System.out.println("Hola mundo2 " + nombre);
+
+	}
+	
+	public static int suma(){
+		
+		return 2 + 5;
+		
+	}
+	
+	public static int multiplica(){
+		
+		return 2 * 2;
+		
+	}
+}
